2045c44401437bf514fc6f164f92a1d873e4062e
MarineH <marine.hashan@gmail.com>
2025-04-10 15:48:42 +0200

    Add search feature and templates for book details and book search

--- Library_web/DjangoWebProject/Catalog/urls.py
+++ Library_web/DjangoWebProject/Catalog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'Catalog'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('search/', views.book_search, name='book_search'),
+    path('book/<str:isbn>/', views.book_details, name='book_details'),
 ]
--- Library_web/DjangoWebProject/Catalog/views.py
+++ Library_web/DjangoWebProject/Catalog/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 
-def index(request):
-    return HttpResponse("Welcome to the catalog !")
+# Search for author or title in the Book database
+def book_search(request):
+    query = request.GET.get('q', '')  
+    books = Book.objects.filter(title__icontains=query) | Book.objects.filter(author__icontains=query)
+    return render(request, 'Catalog/book_search.html', {'books': books, 'query': query})
+
+def book_details(request, isbn):
+    book = get_object_or_404(Book, isbn=isbn)
+    return render(request, 'Catalog/book_details.html', {'book': book})
